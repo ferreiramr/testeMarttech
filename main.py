0ca312a234dd66dfb60d7effd1707fa71874fd68
Marcos Ferreira <ferreira.mr@pm.me>
2021-06-08 16:14:07 -0300

    requeriments.txt adicionado

--- main.py
+++ main.py
@@ -7,9 +7,17 @@ app = FastAPI()
 
 notas = {}
 
+@app.get("/")
+def home():
+    return {"Projeto": "Teste Martech",
+            "Autor" : "Marcos Ferreira",
+            "Situação do Projeto": "Impletamenta as funções para adicinar e listar notas"}
+
 @app.post("/notas/nova_nota/{anotacao: Text}")
 def nova_nota(anotação: Text):
-
+    """
+    Adiciona uma nova nota
+    """
     nova_nota = Nota(anotação)
     
     notas[nova_nota.id] = nova_nota
@@ -24,6 +32,9 @@ def atualizar_nota():
 
 @app.get("/notas/todas_as_notas")
 def todas_as_notas():
+    """
+    Exibe todas as notas
+    """
     return notas
 
 @app.get("/notas/nota/{id: uuid}")
